3ac676a1ed81aa8950b55d514eb8dc77e59d9609
kimgyuha <kimgyuha25@gmail.com>
2024-06-17 19:18:30 +0900

    setting :: project setting

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Coble',
+  description: '블록 코딩으로 교육을! Coble',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
