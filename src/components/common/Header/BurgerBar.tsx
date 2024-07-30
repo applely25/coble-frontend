dd392f22f0dc415eeb0e5915b5b25fff0865c4ac
kimgyuha <kimgyuha25@gmail.com>
2024-07-30 15:48:42 +0900

    fix :: nav buger layout

--- src/components/common/Header/BurgerBar.tsx
+++ src/components/common/Header/BurgerBar.tsx
@@ -78,35 +78,33 @@ const SideBarContainer = styled.div`
   top: 0;
   position: fixed;
   transition: 0.5s ease;
-
   &.open {
     right: 0;
     transition: right 0.5s;
   }
-
   ${flex.COLUMN_FLEX}
   padding-bottom: 16px;
   gap: 16px;
-
   background-color: ${theme.extra.white};
   padding: 16px 5vw;
 `;
 
 const Header = styled.div`
-  ${flex.COLUMN_BETWEEN}
+  ${flex.BETWEEN}
   width: 100%;
 `;
 
 const ContentContainer = styled.div`
   width: 100%;
+  ${flex.COLUMN_FLEX}
+  justify-content: space-between;
   height: 100%;
-  ${flex.COLUMN_BETWEEN}
   ul {
     ${flex.COLUMN_FLEX}
     gap: 20px;
   }
   li {
-    ${flex.COLUMN_BETWEEN}
+    ${flex.BETWEEN}
     ${font.B2}
     color: ${theme.gray[400]};
   }
