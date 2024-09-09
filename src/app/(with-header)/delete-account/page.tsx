ed7fe1d8605c926003070d735d65d82f0a4fd053
chlgkdms <haeun3882@gmail.com>
2024-09-09 18:23:38 +0900

    feat :: delete user

--- src/app/(with-header)/delete-account/page.tsx
+++ src/app/(with-header)/delete-account/page.tsx
@@ -1,10 +1,44 @@
 'use client';
 import { Sidebar } from '@/components/page/mypage';
-import { flex, theme } from '@/styles';
+import useInputForm, { PlaceholderKeys } from '@/hooks/useInputForm';
+import { design, flex, font, theme } from '@/styles';
+import color from '@/styles/theme';
 import { styled } from '@linaria/react';
 import Image from 'next/image';
+import Input from '@/components/common/Input';
+import { useMutation } from '@tanstack/react-query';
+import { deleteUserApi } from '@/api/users';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
 
+const inputInitialData: PlaceholderKeys[] = ['password'];
+const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
 export default function DeleteAccount() {
+  const { inputValue, onChange, placeholder } = useInputForm(inputInitialData);
+  const nav = useRouter();
+
+  const { mutate: deleteUserMutate } = useMutation({
+    mutationFn: (data: { password: string }) => deleteUserApi(data.password),
+    mutationKey: ['deleteUser'],
+    onSuccess: (data) => {
+      toast('회원탈퇴가 완료되었습니다.');
+      // todo : 토큰 초기화하기
+      nav.push('/');
+    },
+  });
+
+  const onClickSuccess = () => {
+    if (!passwordRegex.test(inputValue.password)) {
+      toast('비밀번호가 형식에 맞지 않습니다.');
+    } else if (!passwordRegex.test(inputValue.password)) {
+      toast('비밀번호가 형식에 맞지 않습니다.');
+    } else if (!inputValue.password) {
+      toast('비밀번호를 입력해주세요.');
+    } else {
+      deleteUserMutate(inputValue);
+    }
+  };
+
   return (
     <Container>
       <BackgroundImage>
@@ -18,11 +52,69 @@ export default function DeleteAccount() {
         <SidebarWrapper>
           <Sidebar />
         </SidebarWrapper>
-        <ContentChildren></ContentChildren>
+        <ContentChildren>
+          <SectionTitle>회원탈퇴</SectionTitle>
+          <Description>비밀번호 입력 후 탈퇴할 수 있습니다.</Description>
+          <DeleteInputContainer>
+            {inputInitialData.map((key) => (
+              <Input
+                name={key}
+                value={inputValue[key]}
+                placeholder={placeholder[key]}
+                key={key}
+                type={key}
+                onChange={onChange}
+              />
+            ))}
+          </DeleteInputContainer>
+          <DeleteButtonContainer>
+            <DeleteButton onClick={onClickSuccess}>회원 탈퇴하기</DeleteButton>
+          </DeleteButtonContainer>
+        </ContentChildren>
       </ContentContainer>
     </Container>
   );
 }
+
+const DeleteInputContainer = styled.div`
+  ${flex.COLUMN_FLEX}
+  width: 448px;
+`;
+
+const DeleteButton = styled.button`
+  width: 448px;
+  padding: 4px 16px;
+  border-radius: 16px;
+  border: 1px solid ${theme.extra.red};
+  color: ${theme.extra.red};
+  transition:
+    background-color 0.2s,
+    color 0.2s;
+  padding: 16px;
+
+  &:hover {
+    background-color: ${theme.extra.red};
+    color: ${theme.extra.white};
+  }
+`;
+
+const DeleteButtonContainer = styled.div`
+  ${flex.COLUMN_FLEX}
+  margin-top: 50px;
+  width: 448px;
+`;
+
+const Description = styled.p`
+  ${font.B1};
+  color: ${color.gray[500]};
+  padding-top: 4px;
+  margin-bottom: 50px;
+`;
+
+const SectionTitle = styled.h2`
+  ${font.H1};
+  color: ${color.extra.red};
+`;
 
 const SidebarWrapper = styled.div`
   width: 250px;
@@ -57,5 +149,10 @@ const ContentContainer = styled.div`
 `;
 
 const ContentChildren = styled.div`
-  flex: 1;
+  ${flex.COLUMN_CENTER}
+  padding: 32px;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  overflow: hidden;
 `;
